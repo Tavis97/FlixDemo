6f64076c0cce0aeee37f765e22a6ddb16f333e76
Tavis Thompson <tavis97@fb.com>
2017-06-23 18:19:26 -0700

    Completed the collection view and the search bar

--- FlixDemo/MovieViewController.swift
+++ FlixDemo/MovieViewController.swift
@@ -9,29 +9,34 @@
 import UIKit
 import AlamofireImage
 
-class MovieViewcontroller: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+class MovieViewcontroller: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
   
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var SearchBar: UISearchBar!
     var movies: [[String:Any]] = []
     var refreshControl: UIRefreshControl!
+    var filteredMovies: [[String:Any]] = []
     
     
     override func viewDidLoad() {
         
         
         super.viewDidLoad()
-        
+       
         activityIndicator.startAnimating()
         tableView.dataSource = self
+        tableView.delegate = self
+        SearchBar.delegate = self
         self.navigationController?.navigationBar.barTintColor = UIColor.black
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.red]
         refreshControl = UIRefreshControl()
+        fetchMovies()
         
         refreshControl.addTarget(self, action: #selector(MovieViewcontroller.didPullToRefresh(_:)), for: UIControlEvents.valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         
-        fetchMovies()
+        
        
         
     }
@@ -47,7 +52,7 @@ class MovieViewcontroller: UIViewController, UITableViewDataSource, UISearchBarD
     
     func fetchMovies(){
         let url = URL(string:
-            "https://api.themoviedb.org/3/movie/popular?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+            "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         let request = URLRequest(url: url,cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         
@@ -61,24 +66,26 @@ class MovieViewcontroller: UIViewController, UITableViewDataSource, UISearchBarD
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: [])  as! [String:Any]
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 self.movies = movies
+                self.filteredMovies = movies
                 self.tableView.reloadData()
                 self.refreshControl.endRefreshing()
                 self.activityIndicator.stopAnimating()
+                print ("Network Request Recievied")
             }
         }
         task.resume()
-
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        print (filteredMovies.count)
+        return filteredMovies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
         
-        let movie = movies[indexPath.row]
+        let movie = filteredMovies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         cell.titleLabel.text = title
@@ -88,10 +95,7 @@ class MovieViewcontroller: UIViewController, UITableViewDataSource, UISearchBarD
         let  baseURLString  = "https://image.tmdb.org/t/p/w500"
         let posterURL = URL(string:baseURLString + posterPathString)!
         cell.posterImageView.af_setImage(withURL: posterURL)
-        
     
-        
-        
         
         return cell
     }
@@ -99,7 +103,7 @@ class MovieViewcontroller: UIViewController, UITableViewDataSource, UISearchBarD
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell = sender as! UITableViewCell
         if let indexPath = tableView.indexPath(for: cell){
-        let movie = movies[indexPath.row]
+        let movie = filteredMovies[indexPath.row]
         let detailViewController = segue.destination as! DetailViewController
         detailViewController.movie = movie
         }
@@ -110,6 +114,21 @@ class MovieViewcontroller: UIViewController, UITableViewDataSource, UISearchBarD
         // Dispose of any resources that can be recreated.
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText.isEmpty{
+            filteredMovies = movies
+        }
+        else {
+            //create a smaller array of the movies based on whats typed in
+            
+            filteredMovies = movies.filter{ (movie:[String:Any]) -> Bool in
+                let title = movie["title"] as! String
+                return title.range(of: searchText, options: .caseInsensitive,range: nil, locale: nil ) != nil
+        }
+        tableView.reloadData()
+    }
+        
     
-    
+}
 }
---
+++ FlixDemo/posterCell.swift
@@ -0,0 +1,14 @@
+//
+//  posterCell.swift
+//  FlixDemo
+//
+//  Created by Tavis Thompson on 6/23/17.
+//  Copyright © 2017 Tavis Thompson. All rights reserved.
+//
+
+import UIKit
+
+class posterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
